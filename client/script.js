81470be87b4be0d824df5845f3f6c5472e895144
danyalakhtar-github <114396334+danyalakhtar-github@users.noreply.github.com>
2023-03-02 16:45:23 +0530

    updated server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://danyai.onrender.com/', {
+    const response = await fetch('https://danai-ouvl.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
